4fdc09aeed021dd0698c405164d090b99ebd94f4
Tellvinch <tellvinchimani@gmail.com>
2020-06-22 13:27:03 +0300

    new changes

--- app/auth/views.py
+++ app/auth/views.py
@@ -21,23 +21,18 @@ def register():
     return render_template('auth/register.html', form = form)
 
 
-@auth.route('/login', methods = ["GET","POST"])
+@auth.route('/login',methods=['GET','POST'])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email = form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
-            
-            return redirect(url_for('main.index'))
-        else:
-            flash("Login Unsuccessful.Check Email and Password")
-        
-        
-    
-    return render_template('auth/login.html', form = form)
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or Password')
+        # mail_message("Welcome to JVUNE","email/welcome_user",user.email,user=user)
+    title = "Illusion login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/logout')
 def logout():
--- app/models.py
+++ app/models.py
@@ -1,134 +1,84 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
-from flask_login import UserMixin
+from flask_login import UserMixin,current_user
 from . import login_manager
 from datetime import datetime
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
-    
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(20), nullable = False)
-    email = db.Column(db.String(120), nullable = False)
-    password = db.Column(db.String(255), nullable = False)
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    secure_password = db.Column(db.String(255), nullable=False)
     profile_pic_path = db.Column(db.String(255))
-    blogs = db.relationship('Blog', backref = 'author', lazy = True) 
-    
+    blogs = db.relationship('Blog', backref='author', lazy=True)
+
     def save_user(self):
         db.session.add(self)
         db.session.commit()
-    
+    @property
+    def set_password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @set_password.setter
+    def password(self, password):
+        self.secure_password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.secure_password, password)
+
     def ___repr__():
         return f"User ('{self.username}', '{self.email}'')"
-    
+
+
 class Blog(db.Model):
     __tablename__ = 'blogs'
-    
-    id = db.Column(db.Integer, primary_key = True)
+
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255))
     content = db.Column(db.String(255))
-    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     # comments = db.relationship('Comments', backref = 'author' , lazy = True)
-    
-    
+
     def save_blog(self):
         db.session.add(self)
         db.session.commit()
-    
-    
+
     def ___repr__():
         return f"Blog ('{self.title}', '{self.date_posted}'')"
-    
+
 
 # class Comments(db.Model):
 #     __tablename__ = 'comments'
-    
+
 #     id = db.Column(db.Integer, primary_key = True)
 #     comment = db.column(db.String(255))
 #     blog_id = db.Column(db.Integer, db.ForeignKey("blogs.id"))
 #     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    
+
 #     def save_comment(self):
 #         db.session.add(self)
 #         db.session.commit()
-        
-        
+
+
 #     def ___repr__(self):
 #         return f"Comments('{self.comment}')"
-        
 
-    
+
 class Quote:
-   '''
-   quote class to define quote object
-   '''
-   def __init__(self,quote,author):
-       self.quote = quote
-       self.author = author
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-    
-    
-    
-    
-    
-    
-    
+    '''
+    quote class to define quote object
+    '''
 
-    
-    
-    
-    
-    
-    
-    
-
-
-    
+    def __init__(self, quote, author):
+        self.quote = quote
+        self.author = author
